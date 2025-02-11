713796ee7f243d8922a7aa6ad934dd986badf035
AustineNamayi21 <austine.namayi@strathmore.edu>
2025-02-11 20:36:12 +0300

    styling updates

--- order_confirmation.php
+++ order_confirmation.php
@@ -42,6 +42,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Confirmation</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="orderconfirmation.css">
 </head>
 <body>
     <div class="container mt-5">
